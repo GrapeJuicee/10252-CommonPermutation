b41ae5def53cdac20521b594713a2c6589537e7b
oscar90y5 <oscarfernandezangulo@gmail.com>
2017-03-15 12:46:59 +0100

    aparentemente resuelto

--- 10252.cpp
+++ 10252.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 
 int main(){
 	std::string a, b, x;
 	while(std::cin >> a >> b){
-		std::cout << "chachito\n";
+		x = "";
+		for(int i = 0; i < a.size(); i++){
+			for(int j = 0; j < b.size(); j++){
+				if(a[i]==b[j]){
+					x+=a[i];
+					b[j]='*';
+					break;
+				}
+			}
+		}
+		std::sort(x.begin(),x.end());
+		std::cout << x << "\n";
 	}
 }
